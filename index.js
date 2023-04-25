b163896a3811a21d70aa37960bba202e03cf0ecc
Rushikesh <rushikeshsharma12@gmail.com>
2023-04-25 18:56:25 +0530

    setting up of the server and connecting to database

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import mongoose from 'mongoose';
+const app = express();
+
+const PORT = 1300;
+
+const CONNECTION_URL = "mongodb+srv://test:test@cluster0.wmpaxgm.mongodb.net/?retryWrites=true&w=majority"; // for non-dockerized app
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    console.log("Connected to DB!");
+  })
+  .catch((error) => {
+    console.log("Couldn't connect");
+    console.log(error);
+  });
+
+app.listen(PORT, () => {
+    console.log("Server started on 1300");
+  });
