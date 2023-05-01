5ade86c2808e395c1e22dd798441e88cbac760e0
hk2257853 <hk2257853@gmail.com>
2023-05-01 17:59:30 +0530

    frontend: initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="bg-gray-200 p-4 ml-1">
+      <h1 className="text-2xl font-bold mb-4">Hello, world!</h1>
+      <p className="text-gray-700">This is a sample React component using Tailwind CSS.</p>
+      <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded">
+        Click me!
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
